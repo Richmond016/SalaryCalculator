4d82e7528acebb1c642f85bbd05444c5ee776900
Gabriela Dhlamini <madhilam@microsoft.com>
2021-02-01 00:23:35 +0200

    Add project files.

---
+++ SalaryCalculator/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalaryCalculator.Models;
+
+namespace SalaryCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new salary());
+        }
+
+        [HttpPost]
+
+        public IActionResult Index(salary pay)
+        {
+            if(pay.sal > 50000)
+            {
+                pay.tax = pay.sal * 10 / 100;
+            }
+            else if(pay.sal > 30000)
+            {
+                pay.tax = pay.sal * 5 / 100;
+            }
+            else
+            {
+                pay.tax = 0;
+            }
+
+            pay.net = pay.sal - pay.tax;
+
+            return View(pay);
+        }
+
+       
+        
+
+ 
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SalaryCalculator/Models/salary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator.Models
+{
+    public class salary
+    {
+
+        public string Name { get; set; }
+
+        public float sal { get; set; }
+
+        public float tax { get; set; }
+
+        public float net { get; set; }
+
+    }
+}
